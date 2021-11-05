593e92b9c2edaf9b77e5e7363d11609954a6e229
SHINILSUB <deathsizes@hotmail.com>
2021-11-05 15:07:56 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }    
 }
-const person = {
-    name: "sean",
-    age: 22,
-    gender: "male"
-};
+const sean = new Human("sean", 35, "male")
 // definite value
 const say = (person: Human): string => {
     return `hello ${person.name}. ${person.age}old. gender is ${person.age}`
 }
-console.log(say(person));
+console.log(say(sean));
 
 export{}
